3fb06ad46a7087f6b55bf89f90a7b0b64d95b7dc
Sachin <sachinbochare94@gmail.com>
2022-09-21 17:17:40 +0530

    done

---
+++ get1/src/main/java/com/exmple/demo/model/Pallavi.java
@@ -0,0 +1,14 @@
+package com.exmple.demo.model;
+
+public class Pallavi {
+private String Name;
+
+public String getName() {
+	return Name;
+}
+
+public void setName(String name) {
+	Name = name;
+}
+
+}
